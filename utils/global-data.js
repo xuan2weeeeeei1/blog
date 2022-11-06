4901da256b82e1b88f8db6e37b22692c13750f9b
魏若瑜 <111034966+xuan2weeeeeei1@users.noreply.github.com>
2022-11-06 11:10:20 +0800

    Update global-data.js

--- utils/global-data.js
+++ utils/global-data.js
@@ -1,13 +1,13 @@
 export const getGlobalData = () => {
   const name = process.env.BLOG_NAME
     ? decodeURI(process.env.BLOG_NAME)
-    : 'Jay Doe';
+    : '玄微（魏若瑜）';
   const blogTitle = process.env.BLOG_TITLE
     ? decodeURI(process.env.BLOG_TITLE)
-    : 'Next.js Blog Theme';
+    : 'oy ih';
   const footerText = process.env.BLOG_FOOTER_TEXT
     ? decodeURI(process.env.BLOG_FOOTER_TEXT)
-    : 'All rights reserved.';
+    : '「以心为鉴 亦可以见光华。」';
 
   return {
     name,
